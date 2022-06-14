268a8f8d132c9c8a57879a248ff20ca704194829
pradeepvermagh <pradeepverma.gh@gmail.com>
2022-06-14 20:02:19 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+// import logo from './logo.svg'
+export default class App extends Component {
+    constructor(){
+        console.log("1");
+        super();
+        this.state = {
+            monsters: []
+        }
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    componentDidMount(){
+        console.log("3");
+        axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then((response)=>{
+            console.log(response.data);
+            this.setState(()=>{
+               return {monsters : response.data}
+            })
+            console.log("4");
+        })
+    }
+
+  render() {
+    console.log("2");
+    return (
+        <>
+            <div>App</div>
+            { this.state.monsters.map((dataw)=>{
+               return <p key={dataw.name}>Hi this is for the {dataw.name} </p>
+            }) }
+            {/* <button onClick={()=>this.setState({string: "bccccc"})}>Chnge the text</button> */}
+            {/* <img src={ logo } alt="" /> */}
+        </>
+    )
+  }
 }
-
-export default App;
